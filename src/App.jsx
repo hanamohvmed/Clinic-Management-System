e4bb27422dc0d7a6a723b576837b4eced3e22171
hanamohvmed <haniaimos6@gmail.com>
2025-08-03 16:58:05 +0300

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Login } from "./pages/Login";
+import Register from "./pages/Register";
+import { WelcomePage } from "./pages/WelcomePage";
 import "./App.css";
 
-export default function App() {}
+const routes = [
+  { path: "/", element: <WelcomePage /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "*", element: <WelcomePage /> }
+];
+
+const router = createBrowserRouter(routes);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
